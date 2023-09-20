22149abb6f469c4eb982ccddaa05d85cd66dc59a
Saikiranreddy23 <114699421+Saikiranreddy23@users.noreply.github.com>
2023-09-19 19:43:52 -0700

    Update WhiteHatStats.js

--- src/WhiteHatStats.js
+++ src/WhiteHatStats.js
@@ -1,190 +1,109 @@
-import React, {useEffect, useRef,useMemo} from 'react';
+import React, { useEffect, useRef } from 'react';
 import useSVGCanvas from './useSVGCanvas.js';
 import * as d3 from 'd3';
 
-//change the code below to modify the bottom plot view
-export default function WhiteHatStats(props){
-    //this is a generic component for plotting a d3 plot
-    const d3Container = useRef(null);
-    //this automatically constructs an svg canvas the size of the parent container (height and width)
-    //tTip automatically attaches a div of the class 'tooltip' if it doesn't already exist
-    //this will automatically resize when the window changes so passing svg to a useeffect will re-trigger
-    const [svg, height, width, tTip] = useSVGCanvas(d3Container);
+export default function GunDeathsBarGraph(props) {
+  const d3Container = useRef(null);
+  const [svg, height, width, tTip] = useSVGCanvas(d3Container);
+  const margin = { top: 20, right: 20, bottom: 40, left: 40 };
 
-    const margin = 50;
-    const radius = 10;
+  useEffect(() => {
+    if (svg === undefined || props.data === undefined) {
+      return;
+    }
 
+    // Prepare data
+    const data = props.data.states;
 
-    //TODO: modify or replace the code below to draw a more truthful or insightful representation of the dataset. This other representation could be a histogram, a stacked bar chart, etc.
-    //this loop updates when the props.data changes or the window resizes
-    //we can edit it to also use props.brushedState if you want to use linking
-    useEffect(()=>{
-        //wait until the data loads
-        if(svg === undefined | props.data === undefined){ return }
+    // Extract state names and gun death counts
+    const plotData = data.map((state) => ({
+      state: state.state,
+      gunDeaths: state.count,
+    }));
 
-        //aggregate gun deaths by state
-        const data = props.data.states;
-        
-        //get data for each state
-        const plotData = [];
-        for(let state of data){
-            const dd = drawingDifficulty[state.abreviation];
-            let entry = {
-                'count': state.count,
-                'name': state.state,
-                'easeOfDrawing': dd === undefined? 5: dd,
-                'genderRatio': state.male_count/state.count,
-            }
-            plotData.push(entry)
-        }
+    // Create scales for x and y axes
+    const xScale = d3
+      .scaleBand()
+      .domain(plotData.map((d) => d.state))
+      .range([margin.left, width - margin.right])
+      .padding(0.1);
 
-        //get transforms for each value into x and y coordinates
-        let xScale = d3.scaleLinear()
-            .domain(d3.extent(plotData,d=>d.easeOfDrawing))
-            .range([margin+radius,width-margin-radius]);
-        let yScale = d3.scaleLinear()
-            .domain(d3.extent(plotData,d=>d.count))
-            .range([height-margin-radius,margin+radius]);
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(plotData, (d) => d.gunDeaths)])
+      .nice()
+      .range([height - margin.bottom, margin.top]);
 
+    // Create bars for the bar graph
+    svg.selectAll('.bar').remove();
+    svg
+      .selectAll('.bar')
+      .data(plotData)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', (d) => xScale(d.state))
+      .attr('y', (d) => yScale(d.gunDeaths))
+      .attr('width', xScale.bandwidth())
+      .attr('height', (d) => height - margin.bottom - yScale(d.gunDeaths))
+      .style('fill', 'darkblue')
+      .on('mouseover', (e, d) => {
+        const text = `${d.state}<br>Gun Deaths: ${d.gunDeaths}`;
+        props.ToolTip.moveTTipEvent(tTip, e);
+        tTip.html(text);
+      })
+      .on('mousemove', (e) => {
+        props.ToolTip.moveTTipEvent(tTip, e);
+      })
+      .on('mouseout', (e) => {
+        props.ToolTip.hideTTip(tTip);
+      });
 
-        //draw a line showing the mean values across the curve
-        //this probably isn't actually regression
-        const regressionLine = [];
-        for(let i = 0; i <= 10; i+= 1){
-            let pvals = plotData.filter(d => Math.abs(d.easeOfDrawing - i) <= .5);
-            let meanY = 0;
-            if(pvals.length > 0){
-                for(let entry of pvals){
-                    meanY += entry.count/pvals.length
-                }
-            }
-            let point = [xScale(i),yScale(meanY)]
-            regressionLine.push(point)
-        }
-        
-        //scale color by gender ratio for no reason
-        let colorScale = d3.scaleDiverging()
-            .domain([0,.5,1])
-            .range(['magenta','white','navy']);
+    // Draw x and y axes
+    svg.selectAll('g').remove();
+    svg
+      .append('g')
+      .attr('class', 'x-axis')
+      .attr('transform', `translate(0,${(height - margin.bottom)})`)
+      .call(d3.axisBottom(xScale))
+      .selectAll('text')
+      .style('text-anchor', 'middle')
+      .attr('transform', 'rotate(-45)') // Rotate x-axis labels for better readability
 
-        //draw the circles for each state
-        svg.selectAll('.dot').remove();
-        svg.selectAll('.dot').data(plotData)
-            .enter().append('circle')
-            .attr('cy',d=> yScale(d.count))
-            .attr('cx',d=>xScale(d.easeOfDrawing))
-            .attr('fill',d=> colorScale(d.genderRatio))
-            .attr('r',10)
-            .on('mouseover',(e,d)=>{
-                let string = d.name + '</br>'
-                    + 'Gun Deaths: ' + d.count + '</br>'
-                    + 'Difficulty Drawing: ' + d.easeOfDrawing;
-                props.ToolTip.moveTTipEvent(tTip,e)
-                tTip.html(string)
-            }).on('mousemove',(e)=>{
-                props.ToolTip.moveTTipEvent(tTip,e);
-            }).on('mouseout',(e,d)=>{
-                props.ToolTip.hideTTip(tTip);
-            });
-           
-        //draw the line
-        svg.selectAll('.regressionLine').remove();
-        svg.append('path').attr('class','regressionLine')
-            .attr('d',d3.line().curve(d3.curveBasis)(regressionLine))
-            .attr('stroke-width',5)
-            .attr('stroke','black')
-            .attr('fill','none');
+    svg
+      .append('g')
+      .attr('class', 'y-axis')
+      .attr('transform', `translate(${margin.left},0)`)
+      .call(d3.axisLeft(yScale).ticks(5));
 
-        //change the title
-        const labelSize = margin/2;
-        svg.selectAll('text').remove();
-        svg.append('text')
-            .attr('x',width/2)
-            .attr('y',labelSize)
-            .attr('text-anchor','middle')
-            .attr('font-size',labelSize)
-            .attr('font-weight','bold')
-            .text('How Hard it Is To Draw Each State Vs Gun Deaths');
+    // Add axis labels and chart title
 
-        //change the disclaimer here
-        svg.append('text')
-            .attr('x',width-20)
-            .attr('y',height/3)
-            .attr('text-anchor','end')
-            .attr('font-size',10)
-            .text("I'm just asking questions");
+    svg
+      .append('text')
+      .attr('x', width / 2)
+      .attr('y', height - 6)
+      .attr('text-anchor', 'middle')
+      .text('State');
+    
+    
+    svg
+      .append('text')
+      .attr('x', -height / 2)
+      .attr('y', 11)
+      .attr('text-anchor', 'middle')
+      .attr('transform', 'rotate(-90)')
+      .text('Gun Deaths');
 
-        //draw basic axes using the x and y scales
-        svg.selectAll('g').remove()
-        svg.append('g')
-            .attr('transform',`translate(0,${height-margin+1})`)
-            .call(d3.axisBottom(xScale))
+    svg
+      .append('text')
+      .attr('x', width / 2)
+      .attr('y', margin.top - 10)
+      .attr('text-anchor', 'middle')
+      .text('Gun Deaths by State');
 
-        svg.append('g')
-            .attr('transform',`translate(${margin-2},0)`)
-            .call(d3.axisLeft(yScale))
-        
-    },[props.data,svg]);
+  }, [props.data, svg]);
 
-    return (
-        <div
-            className={"d3-component"}
-            style={{'height':'99%','width':'99%'}}
-            ref={d3Container}
-        ></div>
-    );
-}
-//END of TODO #1.
-
- 
-const drawingDifficulty = {
-    'IL': 9,
-    'AL': 2,
-    'AK': 1,
-    'AR': 3,
-    'CA': 9.51,
-    'CO': 0,
-    'DE': 3.1,
-    'DC': 1.3,
-    'FL': 8.9,
-    'GA': 3.9,
-    'HI': 4.5,
-    'ID': 4,
-    'IN': 4.3,
-    'IA': 4.1,
-    'KS': 1.6,
-    'KY': 7,
-    'LA': 6.5,
-    'MN': 2.1,
-    'MO': 5.5,
-    'ME': 7.44,
-    'MD': 10,
-    'MA': 6.8,
-    'MI': 9.7,
-    'MN': 5.1,
-    'MS': 3.8,
-    'MT': 1.4,
-    'NE': 1.9,
-    'NV': .5,
-    'NH': 3.7,
-    'NJ': 9.1,
-    'NM': .2,
-    'NY': 8.7,
-    'NC': 8.5,
-    'ND': 2.3,
-    'OH': 5.8,
-    'OK': 6.05,
-    'OR': 4.7,
-    'PA': 4.01,
-    'RI': 8.4,
-    'SC': 7.1,
-    'SD': .9,
-    'TN': 3.333333,
-    'TX': 8.1,
-    'UT': 2.8,
-    'VT': 2.6,
-    'VA': 8.2,
-    'WA': 9.2,
-    'WV': 7.9,
-    'WY': 0,
+  return (
+    <div className={"d3-component"} style={{ 'height': '99%', 'width': '99%' }} ref={d3Container}></div>
+  );
 }
